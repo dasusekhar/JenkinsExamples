0036eb72a86e8250438d3d4fb5369460fb358d5a
Your Name <you@example.com>
2022-02-06 09:59:32 +0530

    first commit

---
+++ src/test/java/com/example/demo/expense/JenkinsExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.expense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
