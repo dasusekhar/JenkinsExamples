6ca922e34e655d150c3eaf45f38931914694b82b
Your Name <you@example.com>
2022-02-06 11:08:15 +0530

    second commit

--- src/main/java/com/example/demo/expense/JenkinsExamplesApplication.java
+++ src/main/java/com/example/demo/expense/JenkinsExamplesApplication.java
@@ -1,12 +1,24 @@
 package com.example.demo.expense;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.classic.Logger;
+
 @SpringBootApplication
 public class JenkinsExamplesApplication {
-
+	
+public static org.slf4j.Logger logger=LoggerFactory.getLogger(JenkinsExamplesApplication.class);
+@PostConstruct
+public void intt()
+{
+	logger.info("application started");
+}
 	public static void main(String[] args) {
+		logger.info("application executed");
 		SpringApplication.run(JenkinsExamplesApplication.class, args);
 	}
 
--- src/test/java/com/example/demo/expense/JenkinsExamplesApplicationTests.java
+++ src/test/java/com/example/demo/expense/JenkinsExamplesApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.demo.expense;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinsExamplesApplicationTests {
-
+	public static org.slf4j.Logger logger=LoggerFactory.getLogger(JenkinsExamplesApplication.class);
 	@Test
 	void contextLoads() {
+		logger.info("test case exexuted");
 	}
 
 }
